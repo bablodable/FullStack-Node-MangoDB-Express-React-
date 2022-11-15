15ddb857e204759eb009784136791f5c434bedb2
Eduard <edmerg13@gmail.com>
2022-11-15 16:15:35 +0300

    auth and logout

--- Frontend/src/components/Header/index.jsx
+++ Frontend/src/components/Header/index.jsx
@@ -1,14 +1,21 @@
 import React from 'react';
 import Button from '@mui/material/Button';
 import { Link } from 'react-router-dom';
+import { useDispatch, useSelector } from 'react-redux';
 
 import styles from './Header.module.scss';
 import Container from '@mui/material/Container';
+import { logout, selectIsAuth } from '../../redux/slices/auth';
 
 export const Header = () => {
-  const isAuth = false;
+  const isAuth = useSelector(selectIsAuth);
+  const dispatch = useDispatch();
 
-  const onClickLogout = () => {};
+  const onClickLogout = () => {
+    if (window.confirm('Exit? You sure?')) {
+      dispatch(logout());
+    }
+  };
 
   return (
     <div className={styles.root}>
